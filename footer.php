c5dddfe403472143b2ac0490d1db1075a7734f04
Ruagof <ruagof@gmail.com>
2015-02-26 14:19:44 +0000

    AWP - the theme
    part 1 - the theme

---
+++ footer.php
@@ -0,0 +1,10 @@
+<?php get_template_part('twitter', 'posts'); ?>
+
+<?php wp_footer() ?>
+
+<div id="footer">
+                <p>&copy; Peter Gough, 2015</p>
+        </div>
+    </div>
+</body> 
+</html>

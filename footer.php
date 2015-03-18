52f97c8ac136fd906270a302daa60812946699f3
Ruagof <ruagof@gmail.com>
2015-03-18 22:08:21 +0000

    Navigation is styled

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-        <?php get_template_part('twitter', 'posts'); ?>
+
 
         <?php wp_footer() ?>
 
---
+++ home.php
@@ -0,0 +1,30 @@
+                    <?php get_header(); ?>
+
+
+                        <div id="main">
+                            <?php get_sidebar('left'); ?>
+
+                               <div id="content">
+
+                                    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>  <!-- The wordpress loop --!>
+                                       <h1><?php the_title(); ?></h1>
+                                       <p class="dateofpost">Posted on <?php the_time('F jS, Y') ?><p>
+                                            <p>Based on index.php</p>
+                                       <p><?php the_content(__('(more...)')); ?></p>
+                                       <p><?php the_tags( 'Related articles: ', ', ', '<br />' ); ?></p>
+                                    <hr/> 
+                                    <?php endwhile; else: ?>
+
+                                        <p><?php _e('Sorry, no posts found 404.'); ?></p>
+
+                                    <?php endif; ?>
+                                  
+                                </div>
+
+                            <?php get_sidebar('right'); ?>
+
+                        </div>
+
+                    <div id="delimiter"></div>
+
+                <?php get_footer(); ?>
